8c958baf52df62baabefc5a8fd094c2e1b52ef99
Tarsum67 <taraum@icloud.com>
2023-11-20 20:52:57 -0600

    update

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -24,7 +24,41 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(
+  // Cache CSS, JS, and images with a Stale While Revalidate strategy
+  ({ request }) =>
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'image',
+  new StaleWhileRevalidate({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
 
-// TODO: Implement asset caching
-registerRoute();
+// Implement asset caching for other resources here
+// Example: Cache fonts with a Cache First strategy
+registerRoute(
+  ({ request }) => request.destination === 'font',
+  new CacheFirst({
+    cacheName: 'fonts-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 365 * 24 * 60 * 60, // Cache for one year
+      }),
+    ],
+  })
+);
+
+// Offline fallback strategy
+offlineFallback();
+
+// This is just a placeholder; you need to provide appropriate routes and strategies for your assets
+// For more details, refer to the Workbox documentation: https://developers.google.com/web/tools/workbox
